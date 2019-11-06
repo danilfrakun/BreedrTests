d4ef73ecfe73c0df57fa6167652d78c487f7b735
Frakun <frakun@Polis.local>
2019-11-06 19:01:31 +0200

    Update emails

--- loginNegWrongPass_test.js
+++ loginNegWrongPass_test.js
@@ -5,7 +5,7 @@ Scenario('check that user gets error message in case of wrong pass', (I) => {
     I.amOnPage('https://app2-staging.breedr.co/signin');
     I.see('Login');
     I.click('Login');
-    I.fillField('email', 'lyubov.bobrovitskaya@litslink.com');
+    I.fillField('email', 'test11171117+farm@gmail.com');
     I.fillField('password', 'qweqwe');
     I.click('Login');
     I.waitForText('Incorrect credentials', 10);
--- loginPos_test.js
+++ loginPos_test.js
@@ -4,7 +4,7 @@ Scenario('check if user can login', (I) => {
     I.amOnPage('https://app2-staging.breedr.co/signin');
     I.see('Login');
     I.click('Login');
-    I.fillField('email', 'lyubov.bobrovitskaya@litslink.com');
+    I.fillField('email', 'test11171117+farm@gmail.com');
     I.fillField('password', 'Alex2312');
     I.click('Login');
     I.waitForText('Trades', 10);
